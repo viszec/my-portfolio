7fdaf5793967e4e16f8755b6852f71968a48a67b
Mavis Ma <s3621134@student.rmit.edu.au>
2024-04-20 23:14:35 +1000

    merge

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 const Frontend = () => {
     return (
         <div className="skills__content">
-<<<<<<< HEAD
+
             <h3 className="skills__title">Frontend developer</h3>
-=======
-            <h3 className="skills__title"></h3>
->>>>>>> 9139be7002d2716759c5832c6fc0d2f18ff9eb6f
+
 
             <div className="skills__box">
                 <div className="skills__group">
@@ -15,13 +13,10 @@ const Frontend = () => {
                         <i class='bx bx-badge-check' ></i>
 
                         <div>
-<<<<<<< HEAD
+
                             <h3 className="skills__name">HTML</h3>
                             <span className="skills__level">Basic</span>
-=======
-                            <h3 className="skill__name"></h3>
-                            <span className="skill__level"></span>
->>>>>>> 9139be7002d2716759c5832c6fc0d2f18ff9eb6f
+
                         </div>
                     </div>
 
@@ -29,13 +24,10 @@ const Frontend = () => {
                         <i class='bx bx-badge-check' ></i>
 
                         <div>
-<<<<<<< HEAD
+
                             <h3 className="skills__name">CSS</h3>
                             <span className="skills__level">Advanced</span>
-=======
-                            <h3 className="skill__name"></h3>
-                            <span className="skill__level"></span>
->>>>>>> 9139be7002d2716759c5832c6fc0d2f18ff9eb6f
+
                         </div>
                     </div>
 
@@ -43,7 +35,7 @@ const Frontend = () => {
                         <i class='bx bx-badge-check' ></i>
 
                         <div>
-<<<<<<< HEAD
+
                             <h3 className="skills__name">JavaScript</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
@@ -77,13 +69,6 @@ const Frontend = () => {
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
-=======
-                            <h3 className="skill__name"></h3>
-                            <span className="skill__level"></span>
-                        </div>
-                    </div>
-
->>>>>>> 9139be7002d2716759c5832c6fc0d2f18ff9eb6f
                 </div>
             </div>
         </div>
