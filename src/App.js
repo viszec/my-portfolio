89cfb300c7a814dcaf72bb8a9edc34b6fca42b8f
Mavis Ma <s3621134@student.rmit.edu.au>
2024-04-20 23:06:05 +1000

    merge

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import Header from './components/header/Header.jsx';
 import Home from './components/home/Home.jsx';
 import About from './components/about/About.jsx';
 import Skills from './components/skills/Skills.jsx';
-<<<<<<< HEAD
 import Services from './components/services/Services.jsx';
 import Qualification from './components/qualification/Qualification.jsx';
 /*import Testimonials from './components/testimonials/Testimonials.jsx'*/
-=======
->>>>>>> 9139be7002d2716759c5832c6fc0d2f18ff9eb6f
+
 
 export const App = () => {
   return (
@@ -21,12 +19,9 @@ export const App = () => {
         <Home />
         <About />
         <Skills />
-<<<<<<< HEAD
         <Services />
         <Qualification />
 
-=======
->>>>>>> 9139be7002d2716759c5832c6fc0d2f18ff9eb6f
       </main>
     </>
   )
