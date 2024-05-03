9113758b68b635f6eb8acc0bca8d6b3b4149cfd5
Mavis Ma <s3621134@student.rmit.edu.au>
2024-05-03 19:14:58 +1000

    upadte portfolio

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,47 +1,49 @@
-import Work1 from "../../assets/work1.jpg";
+import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
+import Work3 from "../../assets/work3.png";
 import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work5 from "../../assets/work5.png";
 
 
 export const projectsData = [
     {
         id: 1,
         image: Work1,
-        title: "Web design",
-        category: "Web",
+        title: "Brighton ELC",
+        category: ["Web"],
+        url: "https://www.brightonelc.com.au/",
     },
     {
         id: 2,
         image: Work2,
         title: "App movil",
-        category: "App",
+        category: ["App"],
     },
     {
         id: 3,
         image: Work3,
         title: "Brand design",
-        category: "Design",
+        category: ["Design","Web"],
+        url:"https://www.pm.astraearlylearning.com.au/",
     },
     {
         id: 4,
         image: Work4,
         title: "App movil",
-        category: "App",
+        category: ["App"],
     },
     {
         id: 5,
         image: Work5,
-        title: "Web design",
-        category: "Web",
+        title: "Oak Park Children's Centre",
+        category: ["Web"],
+        url:"https://oakparkchildrenscentre.com.au/",
     },
 ];
 
 export const projectsNav = [
     {
         name: 'All',
-
     },
     {
         name: 'Web'
